a1a46e20ac041a6a6da4a79e535bc8c67d4af80c
plausibility <chris@gibsonsec.org>
2013-06-09 13:21:47 +1000

    Semi-functional (whoo) event handling with decorators. Needs a cleanup though.

--- strawberries/master.py
+++ strawberries/master.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
 import girclib.irc
 import girclib.client
+import girclib.signals
 from functools import wraps
 
 import strawberries
 import strawberries.routing
 
+import logging
 
-class Bot(girclib.client.BasicIRCClient):
+
+class Bot(girclib.client.IRCClient):
     commands = []
+    events = []
 
-    def command(self, name, params=None, defaults=None, prefix="!"):
+    def __init__(self, *args, **kwargs):
+        super(Bot, self).__init__(*args, **kwargs)
+        sigs = dir(girclib.signals)
+        for e in [f for f in sigs if f.startswith('on_')]:
+            event = getattr(girclib.signals, e)
+            f = self.handle_event(e)
+            event.connect(f, weak=False)
+
+    def command(self, name, params=None, rank=0, prefix=None):
+        prefix = prefix or "!"
         def outer_decorator(f):
             @wraps(f)
             def inner_decorator(*nargs, **kwargs):
@@ -19,6 +32,23 @@ class Bot(girclib.client.BasicIRCClient):
                     nargs = (None,)
                 args = strawberries.routing._parse_args(params, *nargs)
                 return f(**args)
-            self.commands.append((name, inner_decorator,))
+            self.commands.append((name, inner_decorator, params, rank, prefix,))
             return inner_decorator
         return outer_decorator
+
+    def event(self, event):
+        def outer_decorator(f):
+            self.events.append(('on_{0}'.format(event), f))
+            return f
+        return outer_decorator
+
+    def handle_event(self, event):
+        def decorator(*args, **kwargs):
+            calling = []
+            for e in self.events:
+                if event != e[0]:
+                    continue
+                calling.append(e[1])
+            # Send off those events!
+            [f(*args, **kwargs) for f in calling]
+        return decorator
